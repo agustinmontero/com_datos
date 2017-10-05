be3a6834db5eb36a2e8e7179efdb25f1292cdabd
agustinmontero <agustinmontero87@gmail.com>
2017-10-05 12:01:26 -0300

    *

--- tp_3/udp_client.py
+++ tp_3/udp_client.py
@@ -3,11 +3,12 @@ import socket
 UDP_IP = "192.168.0.4"
 UDP_PORT = 13012
 BUFFER_SIZE = 512
-MESSAGE = "Hola server!"
+MESSAGE = 'Hola server!'
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+data_rec = ''
 try:
     data_rec = sock.recvfrom(BUFFER_SIZE)
 except KeyboardInterrupt:
